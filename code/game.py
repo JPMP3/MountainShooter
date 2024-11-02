37e4c31ecf1fb782fa4f14a89bbfdc5586f8e18b
origin <git@github.com:JPMP3/MountainShooter.git>
2024-11-02 16:40:26 -0300

    Creating main loop Connecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from code.Menu import Menu
+import pygame as pyg
 
 class Game:
     def __init__(self):
-        self.window = None
+        pyg.init()
+        self.window = pyg.display.set_mode(size=(700, 580))  # creates the window
 
     def run(self, ):
-        pass
+
+        # keeps the window opened
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+            # check events
+            # for event in pyg.event.get():
+            #     if event.type == pyg.QUIT:
+            #         pyg.quit()  # close the window
+            #         quit()  # close Pygame
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import pygame as pyg
+from code.game import Game
 
-pyg.init()
-window = pyg.display.set_mode(size = (700, 580)) #creates the window
-
-#keeps the window opened
-while True:
-    #check events
-    for event in pyg.event.get():
-        if event.type == pyg.QUIT:
-            pyg.quit() #close the window
-            quit() #close Pygame
+game = Game()
+game.run()
