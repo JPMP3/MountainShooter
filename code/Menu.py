427b30edbf5db31dc7825bf6e99acb20ea0e1750
origin <git@github.com:JPMP3/MountainShooter.git>
2024-11-03 11:48:28 -0300

    Creating menu interactions Creating ABC Class Creating Factory Lvl1 Background implementation: Parallax

--- code/Menu.py
+++ code/Menu.py
@@ -3,8 +3,9 @@
 import pygame as pyg
 from pygame import surface, rect, image
 from pygame.font import Font
+from pygame import event, mixer_music
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, COLOR_WHITE, MENU_OPTION, GAME_NAME
+from code.Const import WIN_WIDTH, COLOR_ORANGE, COLOR_WHITE, COLOR_YELLOW, MENU_OPTION, GAME_NAME
 
 
 class Menu:
@@ -15,19 +16,25 @@ class Menu:
 
     def run(self, ):
 
-        # pyg.mixer_music.load('musica')
-        # pyg.mixer_music.play(-1)  #-1 deixa em looping
+        menuOpt = 0
+
+        pyg.mixer_music.load('./Assets/Menu.mp3') #seleciona a musica
+        pyg.mixer_music.play(-1)  #-1 deixa em looping
 
         while True:
+
             self.window.blit(source=self.surf, dest=self.rect)
             #IMPRIME NOME
             for i in range(len(GAME_NAME)):
                 self.menu_text(50, GAME_NAME[i], COLOR_ORANGE, ((WIN_WIDTH / 2), 50 + 30 * i))
             #IMPRIME OPTIONS
             for i in range(len(MENU_OPTION)):
-                self.menu_text(30, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 180 + 20 * i))
-
+                if i == menuOpt:
+                    self.menu_text(30, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 190 + 25 * i))
+                else:
+                    self.menu_text(30, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 190 + 25 * i))
             pyg.display.flip()
+
 
             #check events
             for event in pyg.event.get():
@@ -35,7 +42,20 @@ class Menu:
                     pyg.quit()  # close the window
                     quit()  # close Pygame
 
-        pass
+                if event.type == pyg.KEYDOWN:
+                    if event.key == pyg.K_DOWN: #DIRECIONA OPT PRA BAIXO DE FORMA CICLICA
+                        if menuOpt < len(MENU_OPTION) - 1:
+                            menuOpt+=1
+                        else:
+                            menuOpt = 0
+                    if event.key == pyg.K_UP: #DIRECIONA OPT PRA CIMA DE FORMA CICLICA
+                        if menuOpt > 0:
+                            menuOpt-=1
+                        else:
+                            menuOpt = len(MENU_OPTION) - 1
+                    if event.key == pyg.K_RETURN: #ENTER
+                        return MENU_OPTION[menuOpt]
+
 
     def menu_text(self, txt_sz: int, txt: str, txt_color: tuple, txt_center_pos: tuple):
         txt_font: Font = pyg.font.SysFont('Roboto', txt_sz)
--- code/game.py
+++ code/game.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from code.Menu import Menu
-from code.Const import WIN_HEIGHT, WIN_WIDTH
+from code.Const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
 import pygame as pyg
+from code.Level import Level
 
 class Game:
     def __init__(self):
@@ -14,5 +15,13 @@ class Game:
         # keeps the window opened
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pyg.quit()
+                quit()
+            else:
+                pass
